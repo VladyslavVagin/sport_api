d4727d626c9380cab2c97ffa9df8a8586ce978dc
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-28 14:30:19 +0000

    Deploying to gh-pages from @ VladyslavVagin/sport_api@8733efd42456d5848f06ae31579734d8b9f9137a 🚀

---
+++ index.f6a34072.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-burger-open]"),r=document.querySelector("[data-burger-close]"),t=document.querySelector("[data-burger]");e.addEventListener("click",(function(){return t.classList.remove("is-hidden")})),r.addEventListener("click",(function(){return t.classList.add("is-hidden")}))}();
+//# sourceMappingURL=index.f6a34072.js.map
