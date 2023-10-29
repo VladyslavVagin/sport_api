3f86d569bcd2e75ad78351a635dafc5f3df6a65f
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-29 12:16:59 +0000

    Deploying to gh-pages from @ VladyslavVagin/sport_api@7897bc6d80cc8154bd5c689a4aa951e0e2382757 🚀

--- index.40578c4c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-burger-open]"),d=document.querySelector("[data-burger-close]"),t=document.querySelector("[data-burger]");e.addEventListener("click",(()=>t.classList.remove("is-hidden"))),d.addEventListener("click",(()=>t.classList.add("is-hidden")));
-//# sourceMappingURL=index.40578c4c.js.map
