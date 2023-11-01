36c8f266658fe7719adbbae8d4fd2ff0b2d3ff14
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-02 00:15:22 +0100

    update

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -2,7 +2,19 @@
 const openBurger = document.querySelector('[data-burger-open]');
 const closeBurger = document.querySelector('[data-burger-close]');
 const burgerMenu = document.querySelector('[data-burger]');
+const buttonUp = document.querySelector('.up-btn-container');
+
+buttonUp.classList.add('is-hidden');
 
 openBurger.addEventListener('click', () => burgerMenu.classList.remove('is-hidden'));
 closeBurger.addEventListener('click', () => burgerMenu.classList.add('is-hidden'));
 
+window.addEventListener('scroll', onScroll);
+
+function onScroll() {
+    if(scrollY > 600) {
+       buttonUp.classList.remove('is-hidden');
+    } else {
+        buttonUp.classList.add('is-hidden');
+    }
+}
